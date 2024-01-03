dad79db12ad6b11758ed05ed751ebf264483cf66
Trung274 <trunksadboi@gmail.com>
2024-01-04 03:22:14 +0700

    still cant fix display error

--- my-app/server.js
+++ my-app/server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
-const bodyparser = require('body-parser')
-const mysql = require('mysql')
-const multer = require('multer')
-const path = require('path')
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const mysql = require('mysql');
+const multer = require('multer');
+const path = require('path');
 
 // use express static folder
-app.use(express.static("C:/Users/admin/PARZIVAL/xampp/htdocs/Upload-image-to-mysql-and-display-using-Nodejs-Express-and-Multer/my-app"))
+app.use(express.static(path.join(__dirname, 'my-app')));
 
 // body-parser middleware use
-app.use(bodyparser.json())
+app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
     extended: true
-}))
+}));
 
 // Database connection
 const db = mysql.createConnection({
@@ -20,24 +20,26 @@ const db = mysql.createConnection({
     user: "root",
     password: "",
     database: "test"
-})
+});
 
 db.connect(function (err) {
     if (err) {
         return console.error('error: ' + err.message);
     }
     console.log('Connected to the MySQL server.');
-})
+});
 
 //! Use of Multer
+const uploadDir = path.join(__dirname, 'images');
+
 var storage = multer.diskStorage({
     destination: (req, file, callBack) => {
-        callBack(null, 'C:/Users/admin/PARZIVAL/xampp/htdocs/Upload-image-to-mysql-and-display-using-Nodejs-Express-and-Multer/my-app/images')     // directory name where save the file
+        callBack(null, uploadDir);
     },
     filename: (req, file, callBack) => {
-        callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+        callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
-})
+});
 
 var upload = multer({
     storage: storage
@@ -88,13 +90,13 @@ app.post("/uploadAvatar", upload.single('avatar'), (req, res) => {
     if (!req.file) {
         console.log("No avatar file upload");
     } else {
-        console.log(req.file.filename)
-        var avatarSrc = 'http://127.0.0.1:3000/images/' + req.file.filename
-        var insertData = `INSERT INTO users_file(file_src, file_type) VALUES ("${avatarSrc}", 'avatar')`
+        console.log(req.file.filename);
+        var avatarSrc = 'http://127.0.0.1:3000/images/' + req.file.filename;
+        var insertData = `INSERT INTO users_file(file_src, file_type) VALUES ("${avatarSrc}", 'avatar')`;
         db.query(insertData, [avatarSrc], (err, result) => {
-            if (err) throw err
-            console.log("Avatar file uploaded")
-        })
+            if (err) throw err;
+            console.log("Avatar file uploaded");
+        });
     }
 });
 
@@ -103,21 +105,21 @@ app.post("/uploadCoverPhoto", upload.single('coverPhoto'), (req, res) => {
     if (!req.file) {
         console.log("No cover photo file upload");
     } else {
-        console.log(req.file.filename)
-        var coverPhotoSrc = 'http://127.0.0.1:3000/images/' + req.file.filename
-        var insertData = `INSERT INTO users_file(file_src, file_type) VALUES ("${coverPhotoSrc}", 'cover_photo')`
+        console.log(req.file.filename);
+        var coverPhotoSrc = 'http://127.0.0.1:3000/images/' + req.file.filename;
+        var insertData = `INSERT INTO users_file(file_src, file_type) VALUES ("${coverPhotoSrc}", 'cover_photo')`;
         db.query(insertData, [coverPhotoSrc], (err, result) => {
-            if (err) throw err
-            console.log("Cover photo file uploaded")
-        })
+            if (err) throw err;
+            console.log("Cover photo file uploaded");
+        });
     }
 });
 
 // route for Home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // create connection
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
